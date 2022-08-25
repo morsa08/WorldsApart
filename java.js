8ded3f5d7d7fa0ddea9f192b8c275a78b047f8d9
morsa08 <86577165+morsa08@users.noreply.github.com>
2022-08-25 13:25:34 -0500

    Testing Load Screen
    A red div should appear fullscreen until all photos in the slideshow have been loaded.

--- java.js
+++ java.js
@@ -2,146 +2,81 @@
 
 
 
+
 document.addEventListener("DOMContentLoaded", () => {
 
 
-  const moon = document.getElementById("moon");
-  const qualityBtn = document.getElementById("qualityBtn");
   var index = 0;
-  var  slide = document.getElementById("slide");
-  const slideArray = slide.childNodes;
-  const images = [];
-  images.push("Pictures/ParkingGarage.jpg");
-  images.push("Pictures/GoldenGateSmall.jpg");
-   images.push("Pictures/Shark.jpg");
-  images.push("Pictures/Electrical-Station-Small.jpg");
-  images.push("Pictures/DallasSmall.jpg");
-  images.push("Pictures/GoldenGateDark.jpg");
-  images.push("Pictures/GraveyardSunsetSmall.jpg");
+  var slide = document.getElementById("slide");
+  if (slide) {
+    const slideArray = slide.childNodes;
+    const photoArray = [];
+    photoArray.push("Pictures/ParkingGarage.jpg");
+    photoArray.push("Pictures/GoldenGateSmall.jpg");
+    photoArray.push("Pictures/Shark.jpg");
+    photoArray.push("Pictures/Electrical-Station-Small.jpg");
+    photoArray.push("Pictures/DallasSmall.jpg");
+    photoArray.push("Pictures/GoldenGateDark.jpg");
+    photoArray.push("Pictures/GraveyardSunsetSmall.jpg");
 
-  function appendImages() {
-    for (i = 0; i < images.length; i++) {
-      newImage = document.createElement("img");
-      newImage.src = images[i];
-      slide.appendChild(newImage);
+    var counter = 0;
+
+   function allLoadedCallback() {
+      document.getElementsByClassName("loadContainer")[0].className = "loadContainerHidden";
     }
+
+   function onLoadCallback() {
+      counter++;
+      if (counter == (slideArray.length -1)) {
+
+        allLoadedCallback();
+      }
+
+    }
+
+
+
+
+
+
+    function appendImages() {
+      for (i = 0; i < photoArray.length; i++) {
+        newImage = document.createElement("img");
+        newImage.src = photoArray[i];
+        slide.appendChild(newImage);
+      }
+    }
+
+    function startSlide() {
+      function fadeIn(slide) {
+        slide.className = ("fade");
+      }
+
+      function fadeOut(slide) {
+        slide.className = ("");
+      }
+
+      fadeOut(slideArray[index]);
+      index++;
+
+      if (index === slideArray.length) {
+        index = 0;
+      }
+
+      fadeIn(slideArray[index]);
+
+      setTimeout(function() {
+          startSlide();
+        },
+        4000);
+    }
+
+    appendImages(photoArray);
+
+
+    for (let x = 0; x< photoArray.length; x++){
+    photoArray[x].onload = onLoadCallback() ;}
+
+    startSlide(slide, index);
   }
-
-  function startSlide() {
-
-    function fadeIn(slide) {
-      slide.className = ("fade");
-    }
-
-    function fadeOut(slide) {
-      slide.className = ("");
-    }
-
-    fadeOut(slideArray[index]);
-    index++;
-
-    if (index === slideArray.length) {
-      index = 0;
-    }
-
-    fadeIn(slideArray[index]);
-
-    setTimeout(function() {
-        startSlide();
-      },
-      4000);
-  }
-
-  appendImages(images);
-  startSlide(slide, index);
-
-
-
-  // CAROUSEL
-
-  document.querySelectorAll(".carousel-container").forEach((carousel) => {
-    insertNumbers(carousel);
-
-    carousel.querySelector(".prev").addEventListener("click", (e) => {
-      minusItem(carousel);
-    });
-
-    carousel.querySelector(".next").addEventListener("click", () => {
-      plusItem(carousel);
-    });
-
-    insertDots(carousel);
-
-    carousel.querySelectorAll(".dot").forEach((dot) => {
-      dot.addEventListener("click", (e) => {
-        let item = Array.prototype.indexOf.call(
-          e.target.parentNode.children,
-          e.target
-        );
-
-        showItems(carousel, item);
-      });
-    });
-
-    showItems(carousel, 0);
-  });
 });
-
-function insertNumbers(carousel) {
-  const length = carousel.querySelectorAll(".item").length;
-  for (let i = 0; i < length; i++) {
-    const nmbr = document.createElement("div");
-    nmbr.classList.add("numbertext");
-    nmbr.innerText = i + 1 + " / " + length;
-
-    carousel.querySelectorAll(".item")[i].append(nmbr);
-  }
-}
-
-function insertDots(carousel) {
-  const dots = document.createElement("div");
-  dots.classList.add("dots");
-
-  carousel.append(dots);
-
-  carousel.querySelectorAll(".item").forEach((elem) => {
-    const dot = document.createElement("div");
-    dot.classList.add("dot");
-
-    carousel.querySelector(".dots").append(dot);
-  });
-}
-
-function plusItem(carousel) {
-  let item = currentItem(carousel);
-
-  carousel
-    .querySelectorAll(".item")[item].nextElementSibling.classList.contains("item") ?
-    showItems(carousel, item + 1) :
-    showItems(carousel, 0);
-}
-
-function minusItem(carousel) {
-  let item = currentItem(carousel);
-
-  carousel.querySelectorAll(".item")[item].previousElementSibling != null ?
-    showItems(carousel, item - 1) :
-    showItems(carousel, carousel.querySelectorAll(".item").length - 1);
-}
-
-function currentItem(carousel) {
-  return [...carousel.querySelectorAll(".item")].findIndex(
-    (item) => item.style.display == "block"
-  );
-}
-
-function showItems(carousel, item) {
-  if (carousel.querySelectorAll(".item")[currentItem(carousel)] != undefined)
-    carousel.querySelectorAll(".item")[currentItem(carousel)].style.display =
-    "none";
-  carousel.querySelectorAll(".item")[item].style.display = "block";
-
-  if (carousel.querySelector(".dot.active") != null)
-    carousel.querySelector(".dot.active").classList.remove("active");
-  carousel.querySelectorAll(".dot")[item].classList.add("active");
-}
--- javaPhotos.js
+++ javaPhotos.js
@@ -10,12 +10,16 @@ if (gridContainer) {
   photoArray.push("Pictures/Electrical-Station-Small.jpg");
   photoArray.push("Pictures/DallasSmall.jpg");
   photoArray.push("Pictures/GoldenGateDark.jpg");
-  // photoArray.push("Pictures/GraveyardSunsetSmall.jpg");
+  photoArray.push("Pictures/GraveyardSunsetSmall.jpg");
+  // photoArray.push("Pictures/meSmall.jpg");
 
 for (i=0; i<photoArray.length; i++){
 const newGrid = document.createElement("img");
+var link = document.createElement("a");
 newGrid.className="grid-item";
 newGrid.src = photoArray[i];
+link.href = newGrid.src;
 gridContainer.appendChild(newGrid);
+newGrid.appendChild(link);
 }
 }
